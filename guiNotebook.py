2f9c53729ade562f58af124c192a6a8dad242cb0
Sanjaya <117884188+SanjayaCF@users.noreply.github.com>
2023-12-12 19:21:11 +0700

    Update and rename menu.py to guiNotebook.py

--- guiNotebook.py
+++ guiNotebook.py
@@ -46,14 +46,17 @@ def add_output():
     output = AlgoritmaLRU().simulasi_LRU(references.get(),frames.get())
     label.configure(text=output[0])
     references.delete(0, ctk.END)
+    references.insert(0, "7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1")
     frames.delete(0, ctk.END)
+    frames.insert(0, "3")
     add_table(output[1])
 
 def navigate_to_simulation():
     notebook.select(1) 
     references.delete(0, ctk.END)
+    references.insert(0, "7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1")
     frames.delete(0, ctk.END)
-    add_table(AlgoritmaLRU().simulasi_LRU(references.get(), frames.get())[1])
+    frames.insert(0, "3")
     references.focus_set() 
     frames.focus_set()
 
@@ -63,13 +66,23 @@ root.title("Aplikasi Simulasi Algoritma LRU")
 
 notebook = ttk.Notebook(root)
 
-opening_frame = ctk.CTkFrame(notebook)
-notebook.add(opening_frame, text="Opening Page")
+page_intro = ctk.CTkFrame(notebook)
+notebook.add(page_intro, text="Opening Page")
 
-intro_label = ctk.CTkLabel(opening_frame, text="Welcome to the LRU Simulation App!", font=ctk.CTkFont(size=18))
+intro_label = ctk.CTkLabel(page_intro, text="Tugas Akhir Mata Kuliah Sistem Operasi", font=ctk.CTkFont(size=30, weight="bold"))
 intro_label.pack(pady=20)
+kelompok = '''
+Rendy Ananta Kristanto - 71220840
+Vittorio Emmanuel Harianto - 71220912
+Leif Sean Kusumo - 71220915
+Sanjaya Cahyadi Fuad - 71220965
+'''
+perkenalan = ctk.CTkLabel(page_intro, text=kelompok, font=('Arial', 12))
+perkenalan.configure(text_color='white', font=('Courier',20), corner_radius = 5)
+perkenalan.pack()
 
-navigate_button = ctk.CTkButton(opening_frame, text="Start Simulation", width=500, command=navigate_to_simulation)
+navigate_button = ctk.CTkButton(page_intro, text="Start", width=500, command=navigate_to_simulation)
+navigate_button.configure(hover_color='#ADD8E6', border_width=2)
 navigate_button.pack(pady=20)
 
 simulation_frame = ctk.CTkFrame(notebook)
@@ -85,6 +98,7 @@ frames = ctk.CTkEntry(simulation_frame, placeholder_text="Frames")
 frames.pack(fill="x")
 
 add_button = ctk.CTkButton(simulation_frame, text="Simulasi Jadwal", width=500, command=add_output)
+add_button.configure(hover_color='#ADD8E6', border_width=2)
 add_button.pack(pady=20)
 
 label = ctk.CTkLabel(simulation_frame, text="")
